ceaf87728f94b3af3775644c204eb8ae32fa85c2
seanmh <shoban@alumni.nd.edu>
2018-12-12 21:34:35 -0600

    continued commenting in analysis file (which now has only analysis, not sampling code)

--- BSAS_analysis_2.R
+++ BSAS_analysis_2.R
@@ -1,116 +1,10 @@
 
-setwd("/home/user/Dropbox/Projects/IN_PROGRESS/Best_sampling_across_systems/")
-
-library(adegenet); library(diveRsity)
-library(parallel); library(doParallel) #will load foreach
-source("sample_funcs_BSAS.R"); source("src/arp2gen_edit.R")
-colMax <- function(data) sapply(data, max, na.rm = TRUE)
-
-#############################################################
-#	PREP GENPOP FILES AND EMPTY RESULTS MATRICES			#
-#############################################################
-
-#Switch the comment to the other line if doing the stepping stone migration model 
-#scenarios_ran<-list.dirs(path = "./mss_migr_model/", full.names = TRUE,recursive=F)
-scenarios_ran<-list.dirs(path = "./Simulations6/", full.names = TRUE,recursive=F)
-
-#This will convert all arp files to .gen (genepop) files
-#If re-doing this you want to uncomment the first two lines which will wipe all .gen files
-#If you want to wipe all .arp files after the conversion (to save disk space), uncomment the last line
-for (scen in 1:length(scenarios_ran)){	
-#	gen_files<-dir(path=scenarios_ran[scen],pattern="gen")
-#	if (length(gen_files)!=0)  file.remove(file.path(scenarios_ran[scen],gen_files))
-	reps_ran_arp<-list.files(scenarios_ran[scen], pattern="arp")
-	arp_file_list<-file.path(scenarios_ran[scen],reps_ran_arp,sep="")
-	if (.Platform$OS.type=="unix") arp_file_list<-substr(arp_file_list,1,nchar(arp_file_list)-1)
-	mclapply(arp_file_list,arp2gen,mc.cores=16)
-	#this deletes the arp files! DANGER!
-	#file.remove(arp_file_list)
-}
-reps_ran_gen<-list.files(scenarios_ran[scen], pattern="gen")
-	
-#This is the range of sampling efforts to try, from 1 to 250
-n_trees<-1:250
-#Matrix to store results 
-tree_results_temp<-matrix(nrow=length(n_trees),ncol=45)
-all_res<-array(dim=c(length(scenarios_ran),45,length(reps_ran_gen)))
-tree_results_all<-array(dim=c(length(n_trees),45,length(scenarios_ran),length(reps_ran_gen)))
-
-##############################################
-#	SAMPLING LOOP OVER ALL SIMULATION FILES	 #
-##############################################
-
-for (scen in 1:length(scenarios_ran)){	
-	reps_ran_gen<-list.files(scenarios_ran[scen], pattern="gen")
-	num_reps<-length(reps_ran_gen)
-	
-	cl <- makeCluster(28) # create a cluster with X cores
-	registerDoParallel(cl) # register the cluster
-	tree_results_list <- foreach (nrep = 1:num_reps, .packages="adegenet")	%dopar% {
-	#If not using parallel computing, uncomment the below line for normal loop and comment out the above 3 lines
-	#for (nrep in 1:num_reps){
-		print(scenarios_ran[scen])
-
-		#make a genind (by individual) and genpop (by population)
-		temp_file_name<-file.path(scenarios_ran[scen],reps_ran_gen[nrep],sep="")
-			if (.Platform$OS.type=="unix")  temp_file_name<-substr(temp_file_name,1,nchar(temp_file_name)-1)
-		BSAS_genind<-read.genepop(temp_file_name,ncode=3)
-		BSAS_genpop<-genind2genpop(BSAS_genind)
-
-
-		#--NUMBER OF POPULATIONS, INDIVIDUALS, REGIONS, REGIONAL MAKEUP--#
-		n_pops<-length(levels(BSAS_genind@pop))
-		n_total_indivs<- length(BSAS_genind@tab[,1])
-		n_ind_p_pop<-table(BSAS_genind@pop)
-		allele_freqs<-colSums(BSAS_genpop@tab)/(n_total_indivs*2)	
-
-		allele_cat<-get.allele.cat(BSAS_genpop,  n_ind_p_pop,local=F)	
-		#global;			glob_com<-allele_cat;	glob_lowfr<-allele_cat;		glob_rare<-allele_cat
-		#loc_com_int		loc_rare<-allele_cat	somew_com<-allele_cat;		somew_com<-allele_cat;			BM
-
-		BSAS_genind_sep<-seppop(BSAS_genind)
-
-		for (trees_samp in n_trees){
-			all_caught<-matrix(nrow=n_pops,ncol=length(allele_freqs))
-			for (p in 1:n_pops){
-				do_replace<-table(BSAS_genind@pop)[p]<trees_samp
-				if (trees_samp==1) 	all_caught[p,]<-BSAS_genind_sep[[p]]@tab[sample(1:nrow(BSAS_genind_sep[[p]]@tab), trees_samp),]
-				else	all_caught[p,]<-colSums(BSAS_genind_sep[[p]]@tab[sample(1:nrow(BSAS_genind_sep[[p]]@tab), trees_samp,replace=do_replace),])
-			}
-
-			#as long as its not missed (0) it counts as being caught (also try 5, 10, 25, 50)
-			for (l in 1:length(allele_cat)) tree_results_temp[trees_samp,l]<-sum(colSums(all_caught,na.rm=T)[allele_cat[[l]]]>0)
-			for (l in 1:length(allele_cat))	
-					tree_results_temp[trees_samp,l+length(allele_cat)]<-sum(colSums(all_caught,na.rm=T)[allele_cat[[l]]]>=5)
-			for (l in 1:length(allele_cat))	
-					tree_results_temp[trees_samp,l+length(allele_cat)*2]<-sum(colSums(all_caught,na.rm=T)[allele_cat[[l]]]>=10)
-			for (l in 1:length(allele_cat))	
-					tree_results_temp[trees_samp,l+length(allele_cat)*3]<-sum(colSums(all_caught,na.rm=T)[allele_cat[[l]]]>=25)
-			for (l in 1:length(allele_cat))	
-					tree_results_temp[trees_samp,l+length(allele_cat)*4]<-sum(colSums(all_caught,na.rm=T)[allele_cat[[l]]]>=50)
-					
-		}
-			tree_results_temp<-t(t(tree_results_temp)/unlist(lapply(allele_cat,length)))
-			tree_results_temp
-	}
-		gt95<- function(props) min(which(props>0.95))
-			
-		for (nrep in 1: num_reps) all_res[scen,,nrep]<-apply(tree_results_list[[nrep]],2,gt95)
-		for (nrep in 1: num_reps) tree_results_all[,,scen,nrep]<-tree_results_list[[nrep]]
-		stopCluster(cl)
-}
-#Again if running stepping stone model, switch comment out on next lines
-#save(all_res,file="all_res_mss.Rdata") ;   save(tree_results_all,file="tree_results_mss.Rdata")
-save(all_res,file="all_res_mult.Rdata") ;   save(tree_results_all,file="tree_results_mult.Rdata")
-
 
  
 #####################################
 #			ANALYSIS 
 #####################################
 		
-setwd("C:/Users/shoban/Dropbox/Projects/IN_PROGRESS/Best_sampling_across_systems/")
-setwd("C:/Users/shoban.DESKTOP-DLPV5IJ/Dropbox/Projects/IN_PROGRESS/Best_sampling_across_systems/")
 setwd("/home/user/Dropbox/Projects/IN_PROGRESS/Best_sampling_across_systems/")
 load("all_res_mult.Rdata")
 #Remember what is in these results!!!- this is the minumum sampling needed to capture 95% of the alleles in a given category
@@ -119,7 +13,7 @@ load("all_res_mult.Rdata")
 #This replaces all Inf values (never reached 0.95) with NAs
 is.na(all_res) <- sapply(all_res, is.infinite)
 means_caught<-apply(all_res[,,],c(1,2),mean,na.rm=T)
-#This will count the NAs- important to report- NEED TO LOOK AT THIS RESULT AND REPORT
+#This will count the NAs- important to report!!!
 apply(is.na(all_res[,,]),c(1,2),sum)  
 #Most of those with bottleneck of 1 have most reps being Inf/ NAs for local common alleles meaning these just don't exist
 #run this and pull the params and then do table on that to see how many large/ small populations, bottleneck, migr etc.
@@ -139,8 +33,6 @@ lapply(scenarios_ran,get_params)
 sim_params<-matrix(unlist(lapply(scenarios_ran,get_params)),ncol=7,byrow=T)
 sim_params[,5]<-paste(".",sim_params[,5],sep="")
 allele_categories<-c("all alleles (including rare)", "overall common f>0.05", "overall low fr 0.10>f>0.01", 
-					 "overall rare f<0.01", "local1", "local2", "somwhere common f>0.05", "somwhere common f>0.05", "BM overall f~0.05",
-					"all alleles (including rare)", "overall common f>0.05", "overall low fr 0.10>f>0.01", 
 					 "overall rare f<0.01", "local1", "local2", "somwhere common f>0.05", "somwhere common f>0.05", "BM overall f~0.05")
 
 summary(lm(sim_params[,3],means_caught[,2]))
@@ -149,19 +41,24 @@ plot(sim_params[,7],means_caught[,2])
 	
 	
 	
-#PDFs showing each factor for the four kinds of alleles
-	#Can do separately for the three kinds of bottlenecks by looking at rows for means_caught
-#in the inner loop just paste in the 1:392 etc. above for rows of means_caught and sim_params
-#and of course change the file name
-	#Can also do separately for the multiple copies (for example for five copies) 
-#Just change the AT loop i.e. 1,3,8,9 sub for 10,12,17,18
-#and of course change the file name
+#################################
+#	FIGURE 1 AND SUPP PLOTS		#
+#################################
+	
+#PDFs showing each factor for the four kinds of alleles- will reproduce Figure 1
 
-#These are the different bottleneck scenarios simulated... 
-#can copy these rows into the below boxplot code to subset by bottleneck, otherwise will do across all bottlenecks
-sim_paramsB1<-sim_params[1:392,]
-sim_paramsB25<-sim_params[393:784,]
-sim_paramsB5<-sim_params[785:1176,]
+#Can do separately for the three kinds of bottlenecks by looking at rows for means_caught
+	#in the inner loop just paste in the 1:392 etc.
+	#into the below boxplot code to subset by bottleneck, otherwise will do across all bottlenecks
+	#and of course change the file output name to B1, B5, or B25 for bottleneck times
+	#These are the different bottleneck scenarios simulated... 
+	#B1 [1:392,]
+	#B25 [393:784,]
+	#B5 [785:1176,]
+
+#Can also do separately for the multiple copies (for example for five copies) 
+	#Just change the AT loop i.e. 1,3,8,9 sub for 10,12,17,18
+	#and of course change the file name
 
 pdf(file="BSAS_grid_final_1cop.pdf", height=12, width=14)
 par(mfcol=c(4,3), mar=c(2,2,2,2), oma=c(6,5,4,4), cex.axis=2)
@@ -175,7 +72,7 @@ mtext("number of individuals needed", side=2, line=2,outer=T,cex=1.75)
 mtext("Cat 4- species >0.05           Cat 3- local >0.05            Cat 2- 0.01 to 0.10          Cat 1- all alleles       ", side=4, line=2,outer=T,cex=1.4)
 mtext("Number of populations                                 Migration rate                                     Population size", side=1, line=2,outer=T,cex=1.4)
 
-	dev.off()
+dev.off()
 	
 	
 
@@ -187,11 +84,12 @@ mtext("Number of populations                                 Migration rate     
 #The relatively flat boxplots for migration rate and population size suggest that these factors 
 #are dominated by the influence of number of populations which makes sense because the samples are 
 #divided up among the different populations 
-#So we need to control for or parcel out this factor, to see if they have some influence
+#So we need to parcel out these factors, to see if they have some influence
 #Results are PDFs showing the relationship of minimum sampling to factors of migration rate and population size
 #controlling for number of populations (i.e. there are separate graphs for each number of populations)
-#TO CHANGE CODE Change the 7 to a 5 to look at migration rate
-#TO CHANGE CODE Change the 5:8 to 1:4 to look at catch 1 or catch 5
+#COULD CHANGE CODE Change the 7 to a 5 to vary by migration rate
+#COULD CHANGE CODE Change the AT for Allele Type (could look at any alleles or catch multiple)
+
 pdf(file="BSAS_grid_final_psize.pdf", height=12, width=18)
 par(mfcol=c(4,7), mar=c(2,2,2,2), oma=c(4,4,4,4))
 num_pops_char<-unique(sim_params[,3])
@@ -209,7 +107,6 @@ dev.off()
 #################################
 
 #What is the multiplication factor from catch one to catch five
-#BIG RESULT ITS ABOUT 3.5 across the board!!
 #This calculates the ratio of number of samples needed to catch 1 vs. 5 copies
 #across the types of alleles and all scenarios
 pdf(file="mult_factor_boxplots.pdf",height=5,width=15)
