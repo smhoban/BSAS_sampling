836db54bfd5566c916bf6af36d8e9b84f39d3c62
seanmh <shoban@alumni.nd.edu>
2019-03-18 21:28:54 -0500

    editing axes labels for the migration and population size combined boxplots (figure 2), sorting them; edit to y axis

--- BSAS_analysis_2.R
+++ BSAS_analysis_2.R
@@ -91,15 +91,15 @@ dev.off()
 #So we need to parcel out these factors, to see if they have some influence
 #The following will go through every combination of migration rate and population size
 	
-sim_params[sim_params[,7]=="50",7]<-"0050";			sim_params[sim_params[,7]=="75",7]<-"0075";		sim_params[sim_params[,7]=="150",7]<-"0150"
-sim_params[sim_params[,7]=="100",7]<-"0100";		sim_params[sim_params[,7]=="200",7]<-"0200";	sim_params[sim_params[,7]=="300",7]<-"0300"
-sim_params[sim_params[,7]=="400",7]<-"0400";		sim_params[sim_params[,7]=="500",7]<-"0500";		
+sim_params[sim_params[,7]==50,7]<-"  50";			sim_params[sim_params[,7]==75,7]<-"  75";		sim_params[sim_params[,7]==150,7]<-" 150"
+sim_params[sim_params[,7]==100,7]<-" 100";		sim_params[sim_params[,7]==200,7]<-" 200";	sim_params[sim_params[,7]==300,7]<-" 300"
+sim_params[sim_params[,7]==400,7]<-" 400";		sim_params[sim_params[,7]==500,7]<-" 500";		
 
 #This one for publication- focuses on local alleles, has grey for separating each population size
-psize_mig<-paste("p=",sim_params[,7],"  m=0",sim_params[,5],sep="")
+psize_mig<-paste("p=",sim_params[,7],"  m=",sim_params[,5],sep="")
 pdf("vary_migr_by_psize.pdf",height=6,width=13)
 par(mar=c(10,5,3,2))
-AT<-8; boxplot(means_caught[,AT]~psize_mig,las=2,col=c(rep("light grey",7),rep("white",7)),ylab="Nt, number of samples needed per population")
+AT<-8; boxplot(means_caught[,AT]~psize_mig,las=2,col=c(rep("light grey",7),rep("white",7)),ylab="Ni, number of individuals to sample per population")
 dev.off()
 	
 	#This one NOT for publication- has separate page for each allele category- just for examining
@@ -336,14 +336,103 @@ axis(side=4,at=c(0,50,100,150,200,250),labels=F); 	mtext("allele category",side=
 dev.off()
 
 #For reporting, one population, for BM, compare to theoretical expecations
-
 summary(c(all_res[,9,])) #mean of 27.39 median of 28;	mean of 27.9 median of 29	
 sd(c(all_res[,9,]))	#sd 2.67	2.47
 #good, it does conform to expectations of approximately 28 individuals
 
-#and the ratio
+#and the ratio- the "big result" of the paper in terms of capturing multiple alleles- in this case five copies
 mean(means_caught[,10:18]/means_caught[,1:9],na.rm=T)
 #no bn then bn		#[1] 4.824	[1] 4.136
+boxplot(means_caught[,10:18]/means_caught[,1:9])
+
+
+
+#################################################
+# ALLELE FREQUENCY SHIFT and NUMBER ALLELES		#
+#################################################
+
+#This will look at possible explanations for results, in particular for bottleneck and population size
+
+setwd("/home/user/Dropbox/Projects/IN_PROGRESS/Best_sampling_across_systems/")
+library(adegenet); library(diveRsity)
+source("sample_funcs_BSAS.R"); source("src/arp2gen_edit.R")
+
+load("all_res_final.Rdata")
+scenarios_ran<-list.dirs(path = "./Simulations6/", full.names = TRUE,recursive=F)
+#set up the params list
+get_params<- function(scenario) strsplit(strsplit(scenario,"/")[[1]][4],"_")
+sim_params<-matrix(unlist(lapply(scenarios_ran,get_params)),ncol=7,byrow=T)
+sim_params[,5]<-paste(".",sim_params[,5],sep="")
+allele_categories<-c("all alleles (including rare)", "overall common f>0.05", "overall low fr 0.10>f>0.01", 
+					 "overall rare f<0.01", "local1", "local2", "somwhere common f>0.05", "somwhere common f>0.05", "BM overall f~0.05")
+	
+results_allele_shift<-matrix(nrow=length(scenarios_ran),ncol=9)
+
+#We'll do all scenarios and 3 alleles with 3 stats (# alleles, median, .95 quantile) each
+
+for (scen in 1:length(scenarios_ran))
+	{
+	num_reps<-4
+	concat_af<-list(0,0,0)
+	for (nrep in 1:num_reps){
+		reps_ran_gen<-list.files(scenarios_ran[[scen]], pattern="gen")
+		temp_file_name<-file.path(scenarios_ran[[scen]],reps_ran_gen[nrep],sep="")
+			if (.Platform$OS.type=="unix")  temp_file_name<-substr(temp_file_name,1,nchar(temp_file_name)-1)
+		BSAS_genind<-read.genepop(temp_file_name,ncode=3)
+		BSAS_genpop<-genind2genpop(BSAS_genind)
+	
+		n_total_indivs<- length(BSAS_genind@tab[,1])
+		n_ind_p_pop<-table(BSAS_genind@pop)
+		allele_freqs<-colSums(BSAS_genpop@tab)/(n_total_indivs*2)	
+	
+		allele_cat<-get.allele.cat(BSAS_genpop, n_ind_p_pop, local=T)	
+		concat_af[[1]]<-c(concat_af[[1]],allele_freqs[allele_cat[[1]]])		#all alleles
+		concat_af[[2]]<-c(concat_af[[2]],allele_freqs[allele_cat[[3]]])		#low frequency
+		concat_af[[3]]<-c(concat_af[[3]],allele_freqs[allele_cat[[7]]])		#somewhere common
+	}
+results_allele_shift[scen,1:3]<-unlist(lapply(concat_af,length))
+results_allele_shift[scen,4:6]<-unlist(lapply(concat_af,median))
+results_allele_shift[scen,7:9]<-unlist(lapply(concat_af,quantile,.95))
+}
+write.csv(results_allele_shift,"results_allele_shift.csv")
+	
+	#explanation of columns of results table
+	#first three columns are number of alleles for all, low freq, and some_com alleles
+	#next three columns are median frequency for all, low freq, and some_com alleles
+	#next three columns are the .95 quantile of allele frequency for all, low freq, and some_com alleles
+	
+	
+results_allele_shift<-read.csv("results_allele_shift.csv")	
+results_allele_shift<-results_allele_shift[,-1]
+#########################
+#	With bottleneck		#
+#########################
+get_params<- function(scenario) strsplit(strsplit(scenario,"/")[[1]][4],"_") 
+sim_params<-matrix(unlist(lapply(scenarios_ran,get_params)),ncol=7,byrow=T)
+for (i in 1:9){
+	print(c(mean(results_allele_shift[sim_params[,1]=="b1LBSAS",i]),
+	mean(results_allele_shift[sim_params[,1]=="b5LBSAS",i]),
+	mean(results_allele_shift[sim_params[,1]=="b25LBSAS",i])))
+	}
+#for i=1, all alleles ... 157486/201183= 0.783
+#Results for i=4 (all alleles, allele frequency)...	.0384/.032= 1.2
+
+#################################
+#	With population size		#
+#################################
+#(pop size is "100"  "1000" "150"  "200"  "300"  "50"   "500"  "75" ).. double checked this 1/4/2019
+num_mark<-c(10000,	1500,	9000,	8000,	5000,	15000,	4000,	10000)
+boxplot((results_allele_shift[,1]/num_mark)~sim_params[,7])
+for (i in 1:3){
+	print(mean(results_allele_shift[sim_params[,7]=="1000",i])/1500)
+	print(mean(results_allele_shift[sim_params[,7]=="50",i])/15000)
+	}
+#Results for i=1 (all alleles, number of alleles per locus)... 21.01/26.96=0.779
+for (i in 4:9){
+	print(mean(results_allele_shift[sim_params[,7]=="1000",i]))
+	print(mean(results_allele_shift[sim_params[,7]=="50",i]))
+	}
+#Results for i=4 (all alleles, allele frequency)... .0308/.0403=0.764
 
 
 
